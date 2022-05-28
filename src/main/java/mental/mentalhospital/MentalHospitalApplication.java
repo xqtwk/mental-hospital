f9b8fb08d1c7eba8fc2b7248d42b2c1b9121430b
Richard <xqtwkk@gmail.com>
2022-05-28 12:28:33 +0300

    initial commit

---
+++ src/main/java/mental/mentalhospital/MentalHospitalApplication.java
@@ -0,0 +1,13 @@
+package mental.mentalhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mental/mentalhospital/MentalHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package mental.mentalhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
